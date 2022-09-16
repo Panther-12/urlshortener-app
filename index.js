094b6040b2f47166d30de6740ddb3687c3e7bf2f
Nimrod <87605179+Panther-12@users.noreply.github.com>
2022-09-16 08:19:26 +0000

    url creation and redirection routes

--- index.js
+++ index.js
@@ -2,14 +2,45 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let mongoose = require('mongoose');
+const dns = require("dns");
+const bodyParser = require("body-parser");
+
+// connect to the database
+mongoose.connect(process.env['MONGO_URI'], 
+                 {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+// Declare variable to store the url model
+let Url;
+
+// create a schema for the table
+const urlSchema = new mongoose.Schema({
+    original_url:{
+        type: String,
+        unique: true,
+        required: true,
+    },
+    url_code:{
+        type: Number,
+        unique: true
+    }
+})
+// generate model from schema
+Url = mongoose.model("Url", urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+
+// ROUTES
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -18,6 +49,71 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// url post handling
+app.post("/api/shorturl", (req, res)=>{
+    // constant variables
+    const original = req.body.url;
+    const cleaned_url = original.split("/")
+    const code = Math.floor(Math.random()*101);
+
+    // set dns lookup option
+    const options = {
+        family: 4,
+    }
+    
+    // verify the url passed
+    dns.lookup(original.split("/")[2], options, (err, address ,family)=>{
+        if(err){
+            return console.log(err);
+            res.json({
+                error:'invalid url'
+            })
+        } 
+        else{
+            console.log(`Address: ${address}\n Family:${family}`)
+            if(cleaned_url.length === 3){
+                 // create a new url document
+                var new_url = new Url({
+                    original_url: original,
+                    url_code: code
+                })
+                // save the document and return json with the info
+                new_url.save((err, data)=>{
+                    if(err) return console.error(err);
+                    res.json({
+                        original_url: original,
+                        short_url: code
+                    })
+                })
+            }
+            else{
+                res.json({
+                    error:'invalid url'
+                })
+            }
+
+        }
+    })
+
+})
+
+// short site redirect route
+app.get("/api/shorturl/:short_url", (req, res)=>{
+    let params = req.params
+    
+    console.log(params);
+Url.findOne({url_code:parseInt(params.short_url)}, (err, data)=>{
+        if(err){
+            return console.error(err);
+        }
+        else{
+            console.log(data);
+            res.redirect(301, data.original_url);
+        }
+    })
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
